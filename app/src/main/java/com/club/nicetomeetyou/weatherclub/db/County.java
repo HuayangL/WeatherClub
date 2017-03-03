55a183585a40805f30898132cae28b735b5e9e15
Huayang <Huayang>
2017-03-03 11:46:55 +0800

    加入插件数据库和表的各项配置.

---
+++ app/src/main/java/com/club/nicetomeetyou/weatherclub/db/County.java
@@ -0,0 +1,46 @@
+package com.club.nicetomeetyou.weatherclub.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by nice to meet you on 2017/3/3.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String conutyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getConutyName() {
+        return conutyName;
+    }
+
+    public void setConutyName(String conutyName) {
+        this.conutyName = conutyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
